96c2fd1fc91668628486c93481e7a4948326a643
Jennifer Xue <jxue@aclanyc.com>
2017-03-24 00:13:49 +0000

    checking to see if it pushes changes

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
+var len = 50;
 function square(){
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 right(90);
-forward(50);
+forward(len);
 }
 
+function triangle(){
+right(30);
+forward(len);
+right(120);
+forward(len);
+right(120);
+forward(len);
+}
+
+function house(){
+left(90);
+forward(len);
+right(90);
+forward(len);
+right(90);
+forward(len);
+right(90);
+forward(len);
+left(120);
+forward(len);
+left(120);
+forward(len);
+}
+
+var rlen = 100;
+function rectangle(){
+right(60);
+forward(rlen);
+left(90);
+forward(len);
+left(90);
+forward(rlen);
+}
 // Type your function call below
-
-
+square();
+triangle();
 stamp();
+house();
+rectangle();
+
